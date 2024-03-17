f840f8a41364cc8d006bcd8fff2f638cc4428416
Sujal Karmakar <sujalkarmakar6363@gmail.com>
2024-03-17 12:31:11 +0530

    changes in userSchema

---
+++ backend/userSchema.js
@@ -0,0 +1,40 @@
+const mongoose = require("mongoose")
+
+mongoose.connect("mongodb://localhost:27017/paytmDatabse")
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        lowercase: true,
+        minlength: 3,
+        maxlength: 30,
+
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 8
+    },
+    firstName: {
+        type: String,
+        required: true,
+        trim: true,
+        maxlength: 50
+    },
+    lastName: {
+        type: String,
+        required: true,
+        trim: true,
+        maxlength: 50
+    }
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = {
+	User
+};
+
