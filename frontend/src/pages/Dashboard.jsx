75cf7c7ebbae879bfd4f083890dfbb458f4a0f92
Sujalk63 <sujalkarmakar6363@gmail.com>
2024-04-08 02:24:06 -0700

    fixed your balance on dashboard

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,13 +1,40 @@
-import { Appbar } from "../components/AppBar"
-import { Balance } from "../components/Balance"
-import { Users } from "../components/Users"
+import { useState, useEffect } from "react";
+import { Appbar } from "../components/AppBar";
+import { Balance } from "../components/Balance";
+import { Users } from "../components/Users";
+import axios from "axios";
 
 export const Dashboard = () => {
-    return <div>
-        <Appbar />
-        <div className="m-8">
-            <Balance value={"10,000"} />
-            <Users />
-        </div>
+  const [balance, setBalance] = useState(0);
+
+  useEffect(() => {
+    fetchBalance();
+  }, []);
+
+  const fetchBalance = async () => {
+    try {
+      const response = await axios.get(
+        "http://localhost:3000/api/v1/account/balance",
+        {
+          headers: {
+            Authorization: "Bearer " + localStorage.getItem("token"),
+          },
+        }
+      );
+
+      setBalance(response.data.balance);
+    } catch (error) {
+      console.error("Error fetching balance:", error);
+    }
+  };
+
+  return (
+    <div>
+      <Appbar />
+      <div className="m-8">
+        <Balance value={balance} />
+        <Users />
+      </div>
     </div>
-}
+  );
+};
